a3350803cda9715975327aef80c7a553dfeae5c0
Mewlan <iliq@me.com>
2017-10-20 18:11:49 +0800

    Initial commit

---
+++ MVCTempleteModelViewController.c
@@ -0,0 +1,41 @@
+//
+//  MVCTempleteModelViewController.c
+//  MVCTemplete
+//
+//  Created by Mewlan Musajan on 10/20/17.
+//  Copyright (c) 2017 Mewlan Musajan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	model();
+	return 0;
+}
+
+void model()
+{
+	view();
+}
+
+void view()
+{
+	controller();
+}
+
+void outPutView()
+{
+	
+}
+
+void newLine()
+{
+	putchar('\n');
+}
+
+void controller()
+{
+	
+}
+
