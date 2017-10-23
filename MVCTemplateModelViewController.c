93d66aa43d50e59463a64779d2ff16eb55ad69be
Mewlan <iliq@me.com>
2017-10-24 00:43:49 +0800

    impilicite declaration fixed

--- MVCTemplateModelViewController.c
+++ MVCTemplateModelViewController.c
@@ -1,6 +1,6 @@
 //
-//  MVCTempleModelViewController.c
-//  MVCTemple
+//  MVCTemplateModelViewController.c
+//  MVCTemplate
 //
 //  Created by Mewlan Musajan on 10/20/17.
 //  Copyright (c) 2017 Mewlan Musajan. All rights reserved.
@@ -10,22 +10,23 @@
 
 int main(int argc, char const *argv[])
 {
+	void model();
 	model();
 	return 0;
 }
 
 void model()
 {
-
+	void view();
 }
 
 void view()
 {
-
+	void controller();
 }
 
-int controller()
+void controller()
 {
-
+	
 }
 
